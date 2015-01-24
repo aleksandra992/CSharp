c07b8a4db94556106ac2fdc053516f23dce36d5e
aleksandra992 <astojceva@yahoo.com>
2015-01-24 18:46:41 +0200

    update on Conditional Statements

--- Operators and Expressions/6.FourDigitNumber/Program.cs
+++ Operators and Expressions/6.FourDigitNumber/Program.cs
@@ -5,7 +5,13 @@
         static void Main()
         {
             Console.Write("Enter a four digit number : ");
-            int n = int.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
+            while (s[0] == '0' || s.Length!=4)
+            {
+                Console.Write("Enter a four digit number that doesn't start with 0 : ");
+                s = Console.ReadLine();
+            }
+            int n = int.Parse(s);
             int a = (n / 1000);
             int b = (n % 1000 / 100);
             int c = (n % 1000 % 100 / 10);
