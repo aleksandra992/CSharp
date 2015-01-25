9fb8d7293609374d1f17b5e326024f31b031d761
aleksandra992 <astojceva@yahoo.com>
2015-01-25 13:15:46 +0200

    Update IsoscelesTriangle.cs

---
+++ Conditional Statements/12.ZeroSubset/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        string[] integers = Console.ReadLine().Split(' ');
+        int first = int.Parse(integers[0]);
+        int second = int.Parse(integers[1]);
+        int third = int.Parse(integers[2]);
+        int fourth = int.Parse(integers[3]);
+        int fifth = int.Parse(integers[4]);
+        int sum=0;
+        int subsets = 0;
+        string str=" ";
+        string s = " ";
+        
+       
+        for (int i = 1; i < 32; i++)
+        {
+
+            s = Convert.ToString(i, 2).PadLeft(5,'0');
+            
+            int proba = s.Length;
+            for (int j = 0; j < s.Length; j++)
+            {
+                sum +=int.Parse( integers[j]) * int.Parse(s[j].ToString());
+                
+
+                
+            }
+            if (sum == 0)
+            {
+                for (int j = 0; j < s.Length; j++)
+                {
+                    
+                    if (s[j] == '1')
+                    {
+                        str += integers[j] + "+";
+                        //Console.Write(integers[j] + ((j==s.Length-1)?"":"+"));
+                    }
+                   
+
+                }
+                
+                Console.WriteLine("=0\n");
+                    subsets++;
+                sum = 0;
+            }
+            sum = 0;
+
+        }
+        Console.WriteLine(subsets);
+
+
+    }
+}
+
--- Primitive Data Types and Variables/IsoscelesTriangle/IsoscelesTriangle.cs
+++ Primitive Data Types and Variables/IsoscelesTriangle/IsoscelesTriangle.cs
@@ -11,14 +11,36 @@ namespace IsoscelesTriangle
         static void Main()
         {
             char symbol = (char)169;
-            string firstLine = "   " + symbol + "   ";
-            string secondLine = "  " + symbol+" "+symbol + "  ";
-            string thirdLine = " " + symbol+"   "+symbol+ " ";
-            string fourthLine = ""+symbol +" "+ symbol+" "+ symbol+" " + symbol;
-            Console.WriteLine(firstLine);
-            Console.WriteLine(secondLine);
-            Console.WriteLine(thirdLine);
-            Console.WriteLine(fourthLine);
+              int n = int.Parse(Console.ReadLine());
+            int j =1;
+            int m=0;
+
+            string s1, s2;
+
+            for(int i=n;i>1;i--)
+            {
+                s1 = new String(' ', i - 1);
+                s2 = new String(' ', j);
+
+                if (i == n)
+                {
+                    Console.WriteLine(s1 + symbol + s1);
+                   
+                }
+                else
+                {
+                    Console.WriteLine(s1 + symbol + s2 + symbol + s1);
+                    m++;
+                    j = (2*m) + 1;
+                   
+                    
+                }
+
+            }
+
+            s1 = new String(symbol,(n*2) -1);
+            Console.WriteLine(s1);
+        
 
         }
     }
