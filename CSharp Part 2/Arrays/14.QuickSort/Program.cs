5462d6351e541e1e999baeaeef2151aeca3c461b
Aleksandra Stojcheva <astojceva@yahoo.com>
2015-02-13 13:41:37 +0200

    Update on Quick Sort

--- CSharp Part 2/Arrays/14.QuickSort/Program.cs
+++ CSharp Part 2/Arrays/14.QuickSort/Program.cs
@@ -10,6 +10,48 @@ namespace _14.QuickSort
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the first integer array separated with comma");
+            int[] array = Console.ReadLine().Split(new char[] { ',', ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x)).ToArray();
+            QuickSort(array, 0, array.Length - 1);
+            Console.Write(string.Join(",", array));
+        }
+        static void Swap(int[] numbers, int i, int j)
+        {
+            int pom = numbers[i];
+
+            numbers[i] = numbers[j];
+            numbers[j] = pom;
+        }
+        static void QuickSort(int[] elements, int left, int right)
+        {
+            int i, j;
+            int pivot;
+            pivot = elements[(left + right) / 2];
+            i = left;
+            j = right;
+            while (true)
+            {
+                while (elements[i] < pivot) { i++; }
+                while (elements[j] > pivot) { j--; }
+                if (i < j)
+                {
+                    Swap(elements, i, j);
+                    if (elements[i] == elements[j])
+                    {
+                        Swap(elements, i + 1, j);
+                        i++;
+                    }
+
+                }
+                else
+                    break;
+
+            }
+            if (left < i - 1)
+                QuickSort(elements, left, i - 1);
+            if (i + 1 < right)
+                QuickSort(elements, i + 1, right);
         }
     }
 }
