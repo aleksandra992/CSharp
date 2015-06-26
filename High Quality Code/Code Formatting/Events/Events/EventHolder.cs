a178bc0d7d3f8772ccb035b2690bf4b36918fab1
Aleksandra Stojcheva <astojceva@yahoo.com>
2015-06-26 21:41:06 +0300

    High Quality Code/Code Formatting commit

---
+++ High Quality Code/Code Formatting/Events/Events/EventHolder.cs
@@ -0,0 +1,59 @@
+﻿namespace Events
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Wintellect.PowerCollections;
+
+    public class EventHolder
+    {
+        private MultiDictionary<string, Events> byTitle = new MultiDictionary<string, Events>(true);
+        private OrderedBag<Events> byDate = new OrderedBag<Events>();
+
+        public void AddEvent(DateTime date, string title, string location)
+        {
+            Events newEvent = new Events(date, title, location);
+            this.byTitle.Add(title.ToLower(), newEvent);
+            this.byDate.Add(newEvent);
+            Messages.EventAdded();
+        }
+
+        public void DeleteEvents(string titleToDelete)
+        {
+            string title = titleToDelete.ToLower();
+            int removed = 0;
+
+            foreach (var eventToRemove in this.byTitle[title])
+            {
+                removed++;
+                this.byDate.Remove(eventToRemove);
+            }
+
+            this.byTitle.Remove(title);
+            Messages.EventDeleted(removed);
+        }
+
+        public void ListEvents(DateTime date, int count)
+        {
+            OrderedBag<Events>.View eventsToShow = this.byDate.RangeFrom(new Events(date, "", ""), true);
+            int showed = 0;
+
+            foreach (var eventToShow in eventsToShow)
+            {
+                if (showed == count)
+                {
+                    break;
+                }
+
+                Messages.PrintEvent(eventToShow);
+                showed++;
+            }
+
+            if (showed == 0)
+            {
+                Messages.NoEventsFound();
+            }
+        }
+    }
+}
---
+++ High Quality Code/Code Formatting/Events/Events/Events.cs
@@ -0,0 +1,97 @@
+﻿namespace Events
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Events : IComparable
+    {
+        private DateTime date;
+        private String title;
+        private String location;
+
+        public Events(DateTime date, String title, String location)
+        {
+            this.date = date;
+            this.title = title;
+            this.location = location;
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return this.date;
+            }
+
+            set
+            {
+                this.date = value;
+            }
+        }
+
+        public String Title
+        {
+            get
+            {
+                return this.title;
+            }
+
+            set
+            {
+                this.title = value;
+            }
+        }
+
+        public String Location
+        {
+            get
+            {
+                return this.location;
+            }
+
+            set
+            {
+                this.location = value;
+            }
+        }
+
+        public int CompareTo(object obj)
+        {
+            Events other = obj as Events;
+            int byDate = this.Date.CompareTo(other.Date);
+            int byTitle = this.Title.CompareTo(other.Title);
+            int byLocation = this.Location.CompareTo(other.Location);
+            if (byDate == 0)
+            {
+                if (byTitle == 0)
+                {
+                    return byLocation;
+                }
+                else
+                {
+                    return byTitle;
+                }
+            }
+            else
+            {
+                return byDate;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder toString = new StringBuilder();
+            toString.Append(this.Date.ToString("yyyy-MM-ddTHH:mm:ss"));
+            toString.Append(" | " + this.Title);
+            if (this.Location != null && this.Location != String.Empty)
+            {
+                toString.Append(" | " + this.Location);
+            }
+
+            return toString.ToString();
+        }
+    }
+}
