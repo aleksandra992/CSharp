f6a58205872bb56e3356f406e01f9fda92fb0f45
Aleksandra Stojcheva <astojceva@yahoo.com>
2015-07-14 14:44:15 +0300

    Variables, Data, Expressions and Constants Homework.sln commit

---
+++ High Quality Code/Variables, Data, Expressions and Constants Homework/Task 1. Class Size in CSharp/Size.cs
@@ -0,0 +1,36 @@
+﻿namespace SizeOperations
+{
+    using System;
+    /// <summary>
+    /// A Class for size
+    /// </summary>
+    public class Size
+    {
+
+        private double width, height;
+
+        public Size(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width { get; set; }
+        public double Height { get; set; }
+        /// <summary>
+        /// Method for calculation new size, depending on the old size and angle N
+        /// </summary>
+        /// <param name="size">The size that we want to rotate</param>
+        /// <param name="angleOfTheFigureThatWillBeRotated">The degrees of rotation </param>
+        /// <returns>Return object Size with the new width and height</returns>
+        public static Size GetRotatedSize(Size size, double angleOfTheFigureThatWillBeRotated)
+        {
+            double cosinusOfAngle = Math.Cos(angleOfTheFigureThatWillBeRotated);
+            double sinusOfAngle = Math.Sin(angleOfTheFigureThatWillBeRotated);
+            double rotatedHeight = Math.Abs(sinusOfAngle) * size.Width + Math.Abs(cosinusOfAngle) * size.Height;
+            double rotatedWidth = Math.Abs(cosinusOfAngle) * size.Width + Math.Abs(sinusOfAngle) * size.Height;
+            Size rotatedSize = new Size(rotatedWidth, rotatedHeight);
+            return rotatedSize;
+        }
+    }
+}
---
+++ High Quality Code/Variables, Data, Expressions and Constants Homework/Task 2. Method PrintStatistics in CSharp/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace Math
+{
+    using System;
+
+    public class Math
+    {
+        /// <summary>
+        /// Method for max element in array
+        /// </summary>
+        /// <param name="numbers">Array with double values</param>
+        /// <returns>Return the max element in the array</returns>  
+        public static double GetMaxElement(double[] numbers)
+        {
+
+            double maxElement = int.MinValue;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                double number = numbers[i];
+                if (number > maxElement)
+                {
+                    maxElement = number;
+                }
+            }
+            return maxElement;
+
+        }
+
+        /// <summary>
+        /// Method for min element in array
+        /// </summary>
+        /// <param name="numbers">Array with double values</param>
+        /// <returns>Return the min element in the array</returns>
+        public static double GetMinElement(double[] numbers)
+        {
+            double minElement = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                double number = numbers[i];
+                if (number < minElement)
+                {
+                    minElement = number;
+                }
+
+            }
+            return minElement;
+        }
+
+        /// <summary>
+        /// Method for sum of elements in array
+        /// </summary>
+        /// <param name="numbers">Array with double values</param>
+        /// <returns>The sum of the elements</returns>
+        public static double GetSum(double[] numbers)
+        {
+            double sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+        
+        /// <summary>
+        /// Method for average in array
+        /// </summary>
+        /// <param name="numbers">Array with double values</param>
+        /// <returns></returns>
+        public static double GetAverage(double[] numbers)
+        {
+            double average;
+            average = GetSum(numbers) / numbers.Length;
+            return average;
+            
+        }
+
+        /// <summary>
+        /// The method prints Math Statistics of array(min, max and average)
+        /// </summary>
+        /// <param name="arr">Array with double values</param>
+        public static void PrintStatistics(double[] arr)
+        {
+
+            Console.WriteLine("The max element is {0}",GetMaxElement(arr));
+            Console.WriteLine("The min element is {0}",GetMinElement(arr));
+            Console.WriteLine("The average of the elements is {0}",GetAverage(arr));
+        }
+    }
+}
