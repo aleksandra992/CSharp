2f48ee64a4721e8c8633de94dcd8cac594bfbdf0
Aleksandra Stojcheva <astojceva@yahoo.com>
2015-08-21 21:16:41 +0300

    Development Tools Homework added

---
+++ High Quality Code/Development Tools Homework/Tools/Log4net/Log4net/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+namespace Log4net
+{
+    public class Log4net
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Log4net));
+
+        static void Main(string[] args)
+        {
+
+            XmlConfigurator.Configure();
+           
+            Log.Debug("This is a debug message");
+            Log.Warn("This is a warn message");
+            Log.Error("This is a error message");
+            Log.Fatal("This is a fatal message");
+            Console.ReadLine();
+        }
+    }
+}
